a083af9aad8bd53dd6e6b2186c52ce81fcbc762f
ttommyth <ttommyth@gmail.com>
2015-12-04 00:44:23 +0800

    Revert "fix time"
    This reverts commit ca596fc9b46b49698cc006768d047309b4fccd43.

--- public_html/js/Time.js
+++ public_html/js/Time.js
@@ -1,12 +1,3 @@
-function padLeft(str,len){
-    str = '' + str;
-    return str.length >= len ? str : new Array(len - str.length + 1).join("0") + str;
-}
-function padRight(str, len) {
-    str = '' + str;
-    return str.length >= len ? str : str + new Array(len - str.length + 1).join("0");
-}
-
 function Time(time){
     this.hour = parseInt(time.split(":")[0]);
     this.min = parseInt(time.split(":")[1]);
@@ -20,9 +11,9 @@ function Time(time){
         return this.hour+":"+this.min;
     };
     this.toShortTime=function(){
-        return padLeft(this.hour, 2)+"h"+padLeft(this.min, 2)+"m";
+        return this.hour+"h"+this.min+"m";
     };
     this.toSymbolTime=function(){
-        return padLeft(this.hour, 2)+":"+padLeft(this.min, 2);
+        return this.hour+":"+this.min;
     };
 };
